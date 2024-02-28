49810203cf604463cf62cdec22769b81b35e3467
chaimae <chaimaeaafif@gmail.com>
2024-02-28 12:57:38 -0800

    shell loop

--- shell_loop.c
+++ shell_loop.c
@@ -9,38 +9,38 @@
  */
 int shellLoop(info_t *info, char **argv)
 {
-    ssize_t bytesRead = 0;
-    int builtinReturn = 0;
+	ssize_t bytesRead = 0;
+	int builtinReturn = 0;
 
-    while (bytesRead != -1 && builtinReturn != -2)
-    {
-        clearInfo(info);
-        if (check_interactive_mode(info))
-            _puts("$ ");
-        _eputchar(BUFFER_FLUSH);
-        bytesRead = getInput(info);
-        if (bytesRead != -1)
-        {
-            setInfo(info, argv);
-            builtinReturn = findBuiltin(info);
-            if (builtinReturn == -1)
-                findCommand(info);
-        }
-        else if (check_interactive_mode(info))
-            _putchar('\n');
-        freeInfo(info, 0);
-    }
-    writeHistory(info);
-    freeInfo(info, 1);
-    if (!check_interactive_mode(info) && info->status)
-        exit(info->status);
-    if (builtinReturn == -2)
-    {
-        if (info->errNum == -1)
-            exit(info->status);
-        exit(info->errNum);
-    }
-    return (builtinReturn);
+	while (bytesRead != -1 && builtinReturn != -2)
+	{
+		clearInfo(info);
+		if (check_interactive_mode(info))
+			_puts("$ ");
+		_eputchar(BUFFER_FLUSH);
+		bytesRead = getInput(info);
+		if (bytesRead != -1)
+		{
+			setInfo(info, argv);
+			builtinReturn = findBuiltin(info);
+			if (builtinReturn == -1)
+				findCommand(info);
+		}
+		else if (check_interactive_mode(info))
+			_putchar('\n');
+		freeInfo(info, 0);
+	}
+	writeHistory(info);
+	freeInfo(info, 1);
+	if (!check_interactive_mode(info) && info->status)
+		exit(info->status);
+	if (builtinReturn == -2)
+	{
+		if (info->errNum == -1)
+			exit(info->status);
+		exit(info->errNum);
+	}
+	return (builtinReturn);
 }
 
 /**
@@ -54,27 +54,27 @@ int shellLoop(info_t *info, char **argv)
  */
 int findBuiltin(info_t *info)
 {
-    int i, builtinReturn = -1;
-    _builtin_tbl builtinTable[] = {
-        {"exit", exit_shell},
-        {"env", displayEnvironment},
-        {"help", show_help},
-        {"history", display_history},
-        {"setenv", _mysetenv},
-        {"unsetenv", unsetEnvironmentVariable},
-        {"cd", change_directory},
-        {"alias", manage_alias},
-        {NULL, NULL}
-    };
+	int i, builtinReturn = -1;
+	_builtin_tbl builtinTable[] = {
+		{"exit", exit_shell},
+		{"env", displayEnvironment},
+		{"help", show_help},
+		{"history", display_history},
+		{"setenv", _mysetenv},
+		{"unsetenv", unsetEnvironmentVariable},
+		{"cd", change_directory},
+		{"alias", manage_alias},
+		{NULL, NULL}
+	};
 
-    for (i = 0; builtinTable[i].type; i++)
-        if (_strcmp(info->arguments[0], builtinTable[i].type) == 0)
-        {
-            info->lineCount++;
-            builtinReturn = builtinTable[i].func(info);
-            break;
-        }
-    return (builtinReturn);
+	for (i = 0; builtinTable[i].type; i++)
+		if (_strcmp(info->arguments[0], builtinTable[i].type) == 0)
+		{
+			info->lineCount++;
+			builtinReturn = builtinTable[i].func(info);
+			break;
+		}
+	return (builtinReturn);
 }
 
 /**
@@ -85,37 +85,37 @@ int findBuiltin(info_t *info)
  */
 void findCommand(info_t *info)
 {
-    char *commandPath = NULL;
-    int i, countArgs;
+	char *commandPath = NULL;
+	int i, countArgs;
 
-    info->path = info->arguments[0];
-    if (info->lineCountFlag == 1)
-    {
-        info->lineCount++;
-        info->lineCountFlag = 0;
-    }
-    for (i = 0, countArgs = 0; info->arg[i]; i++)
-        if (!is_character_delimiter(info->arg[i], " \t\n"))
-            countArgs++;
-    if (!countArgs)
-        return;
+	info->path = info->arguments[0];
+	if (info->lineCountFlag == 1)
+	{
+		info->lineCount++;
+		info->lineCountFlag = 0;
+	}
+	for (i = 0, countArgs = 0; info->arg[i]; i++)
+		if (!is_character_delimiter(info->arg[i], " \t\n"))
+			countArgs++;
+	if (!countArgs)
+		return;
 
-    commandPath = findPath(info, getEnvironmentVariable(info, "PATH="), info->arguments[0]);
-    if (commandPath)
-    {
-        info->path = commandPath;
-        forkCommand(info);
-    }
-    else
-    {
-        if ((check_interactive_mode(info) || getEnvironmentVariable(info, "PATH=") || info->arguments[0][0] == '/') && isCmd(info, info->arguments[0]))
-            forkCommand(info);
-        else if (*(info->arg) != '\n')
-        {
-            info->status = 127;
-            print_custom_err(info, "not found\n");
-        }
-    }
+	commandPath = findPath(info, getEnvironmentVariable(info, "PATH="), info->arguments[0]);
+	if (commandPath)
+	{
+		info->path = commandPath;
+		forkCommand(info);
+	}
+	else
+	{
+		if ((check_interactive_mode(info) || getEnvironmentVariable(info, "PATH=") || info->arguments[0][0] == '/') && isCmd(info, info->arguments[0]))
+			forkCommand(info);
+		else if (*(info->arg) != '\n')
+		{
+			info->status = 127;
+			print_custom_err(info, "not found\n");
+		}
+	}
 }
 
 /**
@@ -126,34 +126,34 @@ void findCommand(info_t *info)
  */
 void forkCommand(info_t *info)
 {
-    pid_t childPid;
+	pid_t childPid;
 
-    childPid = fork();
-    if (childPid == -1)
-    {
-        perror("Error:");
-        return;
-    }
-    if (childPid == 0)
-    {
-        if (execve(info->path, info->arguments, copyEnvironToStringArray(info)) == -1)
-        {
-            freeInfo(info, 1);
-            if (errno == EACCES)
-                exit(126);
-            exit(1);
-        }
-    }
-    else
-    {
-        wait(&(info->status));
-        if (WIFEXITED(info->status))
-        {
-            info->status = WEXITSTATUS(info->status);
-            if (info->status == 126)
-                print_custom_err(info, "Permission denied\n");
-        }
-    }
+	childPid = fork();
+	if (childPid == -1)
+	{
+		perror("Error:");
+		return;
+	}
+	if (childPid == 0)
+	{
+		if (execve(info->path, info->arguments, copyEnvironToStringArray(info)) == -1)
+		{
+			freeInfo(info, 1);
+			if (errno == EACCES)
+				exit(126);
+			exit(1);
+		}
+	}
+	else
+	{
+		wait(&(info->status));
+		if (WIFEXITED(info->status))
+		{
+			info->status = WEXITSTATUS(info->status);
+			if (info->status == 126)
+				print_custom_err(info, "Permission denied\n");
+		}
+	}
 }
 
 
