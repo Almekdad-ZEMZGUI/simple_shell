c265a09b02ed37c1b351b24f223a46e849e797e7
chaimae <chaimaeaafif@gmail.com>
2024-02-28 13:42:22 -0800

    func1

--- enviroment_func1.c
+++ enviroment_func1.c
@@ -7,8 +7,8 @@
  */
 int displayEnvironment(info_t *info)
 {
-    printStr(info->environment);
-    return (0);
+	printStr(info->environment);
+	return (0);
 }
 
 /**
@@ -20,17 +20,17 @@ int displayEnvironment(info_t *info)
  */
 char *getEnvironmentVariable(info_t *info, const char *name)
 {
-    str_list_t *node = info->environment;
-    char *p;
+	str_list_t *node = info->environment;
+	char *p;
 
-    while (node)
-    {
-        p = _starts_with(node->str, name);
-        if (p && *p)
-            return (p);
-        node = node->next;
-    }
-    return (NULL);
+	while (node)
+	{
+		p = _starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
 }
 
 /**
@@ -41,14 +41,14 @@ char *getEnvironmentVariable(info_t *info, const char *name)
  */
 int _mysetenv(info_t *info)
 {
-    if (info->argumentCount != 3)
-    {
-        _eputs("Incorrect number of arguments\n");
-        return (1);
-    }
-    if (setEnvironmentVariable(info, info->arguments[1], info->arguments[2]))
-        return (0);
-    return (1);
+	if (info->argumentCount != 3)
+	{
+		_eputs("Incorrect number of arguments\n");
+		return (1);
+	}
+	if (setEnvironmentVariable(info, info->arguments[1], info->arguments[2]))
+		return (0);
+	return (1);
 }
 
 /**
@@ -58,17 +58,17 @@ int _mysetenv(info_t *info)
  */
 int unsetEnvironmentVariable(info_t *info)
 {
-    int i;
+	int i;
 
-    if (info->argumentCount == 1)
-    {
-        _eputs("Too few arguments.\n");
-        return (1);
-    }
-    for (i = 1; i <= info->argumentCount; i++)
-        removeEnvironmentVariable(info, info->arguments[i]);
+	if (info->argumentCount == 1)
+	{
+		_eputs("Too few arguments.\n");
+		return (1);
+	}
+	for (i = 1; i <= info->argumentCount; i++)
+		removeEnvironmentVariable(info, info->arguments[i]);
 
-    return (0);
+	return (0);
 }
 
 /**
@@ -78,13 +78,13 @@ int unsetEnvironmentVariable(info_t *info)
  */
 int populateEnvironmentList(info_t *info)
 {
-    str_list_t *node = NULL;
-    size_t i;
+	str_list_t *node = NULL;
+	size_t i;
 
-    for (i = 0; environ[i]; i++)
-        addNodeEnd(&node, environ[i], 0);
-    info->environment = node;
-    return (0);
+	for (i = 0; environ[i]; i++)
+		addNodeEnd(&node, environ[i], 0);
+	info->environment = node;
+	return (0);
 }
 
 
